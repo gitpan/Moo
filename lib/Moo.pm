8572e61e6f932538a7d159a682d67da48879068f
Matt S Trout <perl-stuff@trout.me.uk>
2012-06-27 00:47:27 +0000

    Import of MSTROUT/Moo-0.091011 from CPAN.
    gitpan-cpan-distribution: Moo
    gitpan-cpan-version:      0.091011
    gitpan-cpan-path:         MSTROUT/Moo-0.091011.tar.gz
    gitpan-cpan-author:       MSTROUT
    gitpan-cpan-maturity:     released

--- lib/Moo.pm
+++ lib/Moo.pm
@@ -5,7 +5,7 @@ use Moo::_Utils;
 use B 'perlstring';
 use Sub::Defer ();
 
-our $VERSION = '0.091010'; # 0.91.10
+our $VERSION = '0.091011'; # 0.91.11
 $VERSION = eval $VERSION;
 
 require Moo::sification;
@@ -678,7 +678,9 @@ on your class.
 
 =head1 SUPPORT
 
-IRC: #moose on irc.perl.org
+Users' IRC: #moose on irc.perl.org
+
+Development and contribution IRC: #web-simple on irc.perl.org
 
 =head1 AUTHOR
 
